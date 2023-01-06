fd0feea58a97717370861140d388293e05773fe5
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-01-06 20:45:07 +0100

    Column.js: fix resizing height of windows

--- src/Column.js
+++ src/Column.js
@@ -10,23 +10,13 @@ class Column {
         window.column = this;
         const client = window.client;
 
-        let availableHeight = this.grid.area.height - 2 * GAPS_OUTER.y;
-        const nWindows = this.windows.length;
-        const resizeRatio = (nWindows - 1) / nWindows;
-        for (const windowNode of this.windows.iterator()) {
-            const window = windowNode.item;
-            const windowRect = window.client.frameGeometry;
-            windowRect.height = Math.round(windowRect.height * resizeRatio);
-            availableHeight -= windowRect.height;
-        }
-        client.frameGeometry.height = availableHeight;
-        // TODO: respect min height and unresizable windows
-
         this.windows.insertEnd(window.node);
         if (this.width === null) {
             this.width = client.frameGeometry.width;
         }
         // TODO: also change column width if the new window requires it
+
+        this.resizeWindows();
     }
 
     removeWindow(window) {
@@ -34,5 +24,23 @@ class Column {
         if (this.windows.length === 0) {
             this.grid.removeColumn(this); // TODO: consider doing this in Grid instead
         }
+        this.resizeWindows();
+    }
+
+    resizeWindows() {
+        const nWindows = this.windows.length;
+        if (nWindows === 0) {
+            return;
+        }
+
+        let remainingPixels = this.grid.area.height - 2*GAPS_OUTER.y - (nWindows-1)*GAPS_INNER.y;
+        let remainingWindows = nWindows;
+        for (const windowNode of this.windows.iterator()) {
+            const windowHeight = Math.round(remainingPixels) / remainingWindows;
+            windowNode.item.client.frameGeometry.height = windowHeight;
+            remainingPixels -= windowHeight;
+            remainingWindows--;
+        }
+        // TODO: respect min height and unresizable windows
     }
 }
