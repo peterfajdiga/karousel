8b932cc34d81fd1ab82f0c0148c9d3307bf6b050
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-01-07 12:16:40 +0100

    Window.js: reimplement signal handlers to have access to `this`

--- src/Window.js
+++ src/Window.js
@@ -11,6 +11,7 @@ class Window {
             keepAbove: client.keepAbove,
             keepBelow: client.keepBelow,
         };
+        this.signalHandlers = {};
     }
 
     setRect(x, y, width, height) {
@@ -22,14 +23,32 @@ class Window {
     }
 
     connectToSignals() {
-        this.client.frameGeometryChanged.connect(this.frameGeometryChanged);
+        this.signalHandlers.desktopPresenceChanged = (client, oldDesktop) => {
+            print("client desktopPresenceChanged", client, oldDesktop, client.desktop, client.x11DesktopIds);
+        };
+
+        this.signalHandlers.desktopChanged = () => {
+            print("client desktopChanged", this.client.desktop, this.client.x11DesktopIds);
+        }
+
+        this.signalHandlers.x11DesktopIdsChanged = () => {
+            print("client x11DesktopIdsChanged", this.client.desktop, this.client.x11DesktopIds);
+        }
+
+        this.signalHandlers.frameGeometryChanged = (client, oldGeometry) => {
+            print("client frameGeometryChanged", client, oldGeometry);
+        }
+
+        this.client.desktopPresenceChanged.connect(this.signalHandlers.desktopPresenceChanged);
+        this.client.desktopChanged.connect(this.signalHandlers.desktopChanged);
+        this.client.x11DesktopIdsChanged.connect(this.signalHandlers.x11DesktopIdsChanged);
+        this.client.frameGeometryChanged.connect(this.signalHandlers.frameGeometryChanged);
     }
 
     disconnectFromSignals() {
-        this.client.frameGeometryChanged.disconnect(this.frameGeometryChanged);
-    }
-
-    frameGeometryChanged(client, oldGeometry) {
-        print("client frameGeometryChanged", client, oldGeometry);
+        this.client.desktopPresenceChanged.disconnect(this.signalHandlers.desktopPresenceChanged);
+        this.client.desktopChanged.disconnect(this.signalHandlers.desktopChanged);
+        this.client.x11DesktopIdsChanged.disconnect(this.signalHandlers.x11DesktopIdsChanged);
+        this.client.frameGeometryChanged.disconnect(this.signalHandlers.frameGeometryChanged);
     }
 }
