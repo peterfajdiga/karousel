036832b18a8f08deee2729d6faa6a7edb06e3275
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-01-07 10:13:08 +0100

    signals.js: remove indentation on empty lines

--- src/signals.js
+++ src/signals.js
@@ -2,26 +2,26 @@ const workspaceSignalHandlers = {
     desktopPresenceChanged: (client, desktop) => {
         print("desktopPresenceChanged", client, desktop);
     },
-    
+
     currentDesktopChanged: (desktop, client) => {
         print("currentDesktopChanged", desktop, client);
     },
-    
+
     clientAdded: (client) => {
         print("clientAdded", client);
     },
-    
+
     clientRemoved: (client) => {
         const id = client.windowId;
         if (world.clientMap.has(id)) {
             world.removeClient(id);
         }
     },
-    
+
     clientManaging: (client) => {
         print("clientManaging", client);
     },
-    
+
     clientMinimized: (client) => {
         const id = client.windowId;
         if (world.clientMap.has(id)) {
@@ -29,7 +29,7 @@ const workspaceSignalHandlers = {
             world.minimizedTiled.add(id);
         }
     },
-    
+
     clientUnminimized: (client) => {
         const id = client.windowId;
         assert(!world.clientMap.has(id));
@@ -38,71 +38,71 @@ const workspaceSignalHandlers = {
             world.addClient(id, client);
         }
     },
-    
+
     clientRestored: (client) => {
         print("clientRestored", client);
     },
-    
+
     clientMaximizeSet: (client, h, v) => {
         print("clientMaximizeSet", client, h, v);
     },
-    
+
     killWindowCalled: (client) => {
         print("killWindowCalled", client);
     },
-    
+
     clientActivated: (client) => {
         print("clientActivated", client);
     },
-    
+
     clientFullScreenSet: (client, fullScreen, user) => {
         print("clientFullScreenSet", client, fullScreen, user);
     },
-    
+
     clientSetKeepAbove: (client, keepAbove) => {
         print("clientSetKeepAbove", client, keepAbove);
     },
-    
+
     numberDesktopsChanged: (oldNumberOfDesktops) => {
         print("numberDesktopsChanged", oldNumberOfDesktops);
     },
-    
+
     desktopLayoutChanged: () => {
         print("desktopLayoutChanged");
     },
-    
+
     clientDemandsAttentionChanged: (client, set) => {
         print("clientDemandsAttentionChanged", client, set);
     },
-    
+
     numberScreensChanged: (count) => {
         print("numberScreensChanged", count);
     },
-    
+
     screenResized: (screen) => {
         print("screenResized", screen);
     },
-    
+
     currentActivityChanged: (id) => {
         print("currentActivityChanged", id);
     },
-    
+
     activitiesChanged: (id) => {
         print("activitiesChanged", id);
     },
-    
+
     activityAdded: (id) => {
         print("activityAdded", id);
     },
-    
+
     activityRemoved: (id) => {
         print("activityRemoved", id);
     },
-    
+
     virtualScreenSizeChanged: () => {
         print("virtualScreenSizeChanged");
     },
-    
+
     virtualScreenGeometryChanged: () => {
         print("virtualScreenGeometryChanged");
     },
