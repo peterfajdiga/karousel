370313751d9bb150c5aa0a86ea8961139af3e389
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-01-13 23:17:08 +0100

    shortcuts.js: reorder shortcuts

--- src/shortcuts.js
+++ src/shortcuts.js
@@ -14,6 +14,8 @@ function registerShortcutDbg(title, text, keySequence, callback) {
 }
 
 function registerShortcuts() {
+    registerShortcutDbg("basalt-window-toggle-floating", "Basalt: Toggle floating", "Meta+Space", windowToggleFloating);
+
     registerShortcutDbg("basalt-focus-left", "Basalt: Move focus left", "Meta+A", focusLeft);
     registerShortcutDbg("basalt-focus-right", "Basalt: Move focus right", "Meta+D", focusRight);
     registerShortcutDbg("basalt-focus-up", "Basalt: Move focus up", "Meta+W", focusUp);
@@ -24,16 +26,13 @@ function registerShortcuts() {
     registerShortcutDbg("basalt-window-move-up", "Basalt: Move window up", "Meta+Shift+W", windowMoveUp);
     registerShortcutDbg("basalt-window-move-down", "Basalt: Move window down", "Meta+Shift+S", windowMoveDown);
 
-    registerShortcutDbg("basalt-window-toggle-floating", "Basalt: Toggle floating", "Meta+Space", windowToggleFloating);
-
     registerShortcutDbg("basalt-column-move-left", "Basalt: Move column left", "Meta+Ctrl+Shift+A", columnMoveLeft);
     registerShortcutDbg("basalt-column-move-right", "Basalt: Move column right", "Meta+Ctrl+Shift+D", columnMoveRight);
 
+    registerShortcutDbg("basalt-grid-scroll-left-column", "Basalt: Scroll one column to the left", "Meta+Alt+A", gridScrollLeftColumn);
+    registerShortcutDbg("basalt-grid-scroll-right-column", "Basalt: Scroll one column to the right", "Meta+Alt+D", gridScrollRightColumn);
     registerShortcutDbg("basalt-grid-scroll-left", "Basalt: Scroll left", "Meta+Alt+PgUp", gridScrollLeft);
     registerShortcutDbg("basalt-grid-scroll-right", "Basalt: Scroll right", "Meta+Alt+PgDown", gridScrollRight);
     registerShortcutDbg("basalt-grid-scroll-start", "Basalt: Scroll to start", "Meta+Alt+Home", gridScrollStart);
     registerShortcutDbg("basalt-grid-scroll-end", "Basalt: Scroll to end", "Meta+Alt+End", gridScrollEnd);
-
-    registerShortcutDbg("basalt-grid-scroll-left-column", "Basalt: Scroll one column to the left", "Meta+Alt+A", gridScrollLeftColumn);
-    registerShortcutDbg("basalt-grid-scroll-right-column", "Basalt: Scroll one column to the right", "Meta+Alt+D", gridScrollRightColumn);
 }
