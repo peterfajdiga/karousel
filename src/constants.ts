f0c49f86edd9113f61c846d06ff27d6dafed7ff5
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-01-20 00:48:54 +0100

    constants.ts: bigger side margins

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 const UNATTACH_OFFSET = { x: 24, y: 24 };
-const GAPS_OUTER = { top: 12, bottom: 12, left: 12, right: 12 };
+const GAPS_OUTER = { top: 12, bottom: 12, left: 24, right: 24 };
 const GAPS_INNER = { x: 12, y: 12 };
 const GRID_SCROLL_STEP = 200;
