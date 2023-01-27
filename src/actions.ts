19fe1a20ebfbed9838f760b68fcdfa6d892377d3
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-01-27 13:34:31 +0100

    move functions from client.ts to actions.ts

--- src/actions.ts
+++ src/actions.ts
@@ -321,3 +321,12 @@ function gridScroll(world: World, direction: number) {
     grid.adjustScroll(scrollAmount, false);
     grid.arrange();
 }
+
+function canTile(kwinClient: AbstractClient) {
+    // TODO: support windows on all desktops
+    return !kwinClient.minimized && kwinClient.desktop > 0;
+}
+
+function shouldTile(kwinClient: AbstractClient) {
+    return canTile(kwinClient) && kwinClient.normalWindow && kwinClient.managed && !PREFER_FLOATING.matches(kwinClient);
+}
--- src/clients.ts
+++
@@ -1,8 +0,0 @@
-function canTile(kwinClient: AbstractClient) {
-    // TODO: support windows on all desktops
-    return !kwinClient.minimized && kwinClient.desktop > 0;
-}
-
-function shouldTile(kwinClient: AbstractClient) {
-    return canTile(kwinClient) && kwinClient.normalWindow && kwinClient.managed && !PREFER_FLOATING.matches(kwinClient);
-}
