d2dc73336fd4d24eb390bdb80519ad62a362cea5
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-03-24 16:56:03 +0100

    constants.ts: enlarge gaps

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-const GAPS_OUTER = { top: 12, bottom: 12, left: 12, right: 12 };
-const GAPS_INNER = { x: 12, y: 12 };
-const AUTO_OVERSCROLL_X = 12;
+const GAPS_OUTER = { top: 18, bottom: 18, left: 18, right: 18 };
+const GAPS_INNER = { x: 18, y: 18 };
+const AUTO_OVERSCROLL_X = 18;
 const GRID_SCROLL_STEP = 200;
 const STACKED_BY_DEFAULT = false;
 const PREFER_FLOATING = new Map(Object.entries({
