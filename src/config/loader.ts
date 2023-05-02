ec97c5691f1dddced2091ce7161bb77b7e52bc6d
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-05-02 21:28:54 +0200

    config/loader.ts: remove useless configChanged signal connection

--- src/config/loader.ts
+++ src/config/loader.ts
@@ -3,8 +3,5 @@ function loadConfig() {
     for (const entry of configDef) {
         config[entry.name] = KWin.readConfig(entry.name, entry.default);
     }
-    options.configChanged.connect(() => {
-        console.log("config changed"); // TODO
-    });
     return config;
 }
