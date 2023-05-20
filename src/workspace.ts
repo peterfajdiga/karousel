93ba7168a2a144a284a6805c3437a221737f83c7
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-05-20 10:50:02 +0200

    workspace.ts: remove unneeded logs

--- src/workspace.ts
+++ src/workspace.ts
@@ -1,10 +1,6 @@
 function initWorkspaceSignalHandlers(world: World) {
     const manager = new SignalManager();
 
-    manager.connect(workspace.currentDesktopChanged, (desktop: number, kwinClient: AbstractClient) => {
-        console.log("workspace currentDesktopChanged", desktop, kwinClient);
-    });
-
     manager.connect(workspace.clientAdded, (kwinClient: AbstractClient) => {
         console.assert(!world.hasClient(kwinClient));
         world.addClient(kwinClient);
@@ -15,10 +11,6 @@ function initWorkspaceSignalHandlers(world: World) {
         world.removeClient(kwinClient, true);
     });
 
-    manager.connect(workspace.clientManaging, (kwinClient: X11Client) => {
-        console.log("workspace clientManaging", kwinClient);
-    });
-
     manager.connect(workspace.clientMinimized, (kwinClient: AbstractClient) => {
         world.minimizeClient(kwinClient);
     });
@@ -27,19 +19,11 @@ function initWorkspaceSignalHandlers(world: World) {
         world.unminimizeClient(kwinClient);
     });
 
-    manager.connect(workspace.clientRestored, (kwinClient: X11Client) => {
-        console.log("workspace clientRestored", kwinClient);
-    });
-
     manager.connect(workspace.clientMaximizeSet, (kwinClient: AbstractClient, horizontally: boolean, vertically: boolean) => {
         world.doIfTiled(kwinClient, (window, column, grid) => {
             window.onMaximizedChanged(horizontally, vertically);
             grid.arrange();
         });
-    });
-
-    manager.connect(workspace.killWindowCalled, (kwinClient: X11Client) => {
-        console.log("workspace killWindowCalled", kwinClient);
     });
 
     manager.connect(workspace.clientActivated, (kwinClient: AbstractClient) => {
@@ -60,40 +44,8 @@ function initWorkspaceSignalHandlers(world: World) {
         });
     });
 
-    manager.connect(workspace.clientSetKeepAbove, (kwinClient: X11Client, keepAbove: boolean) => {
-        console.log("workspace clientSetKeepAbove", kwinClient, keepAbove);
-    });
-
     manager.connect(workspace.numberDesktopsChanged, (oldNumberOfDesktops: number) => {
         world.updateDesktops();
-    });
-
-    manager.connect(workspace.desktopLayoutChanged, () => {
-        console.log("workspace desktopLayoutChanged");
-    });
-
-    manager.connect(workspace.clientDemandsAttentionChanged, (kwinClient: AbstractClient, set: boolean) => {
-        console.log("workspace clientDemandsAttentionChanged", kwinClient, set);
-    });
-
-    manager.connect(workspace.numberScreensChanged, (count: number) => {
-        console.log("workspace numberScreensChanged", count);
-    });
-
-    manager.connect(workspace.currentActivityChanged, (id: string) => {
-        console.log("workspace currentActivityChanged", id);
-    });
-
-    manager.connect(workspace.activitiesChanged, (id: string) => {
-        console.log("workspace activitiesChanged", id);
-    });
-
-    manager.connect(workspace.activityAdded, (id: string) => {
-        console.log("workspace activityAdded", id);
-    });
-
-    manager.connect(workspace.activityRemoved, (id: string) => {
-        console.log("workspace activityRemoved", id);
     });
 
     manager.connect(workspace.virtualScreenSizeChanged, () => {
