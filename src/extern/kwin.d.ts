80ecc7e6c9acb386eb6762165396631f4c6c063e
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-08-29 21:31:28 +0200

    define kwin types

--- src/extern/kwin.d.ts
+++ src/extern/kwin.d.ts
@@ -1,6 +1,31 @@
-declare const KWin;
-declare const workspace;
+declare const KWin: {
+    readConfig(key: string, defaultValue: any): any;
+    registerShortcut(name: string, description: string, keySequence: string, callback: () => void): void;
+};
 
+declare const workspace: {
+    PlacementArea: ClientAreaOption;
+
+    activeClient: AbstractClient;
+    desktops: number;
+    currentDesktop: number;
+    currentActivity: string;
+
+    clientAdded: QSignal;
+    clientRemoved: QSignal;
+    clientMinimized: QSignal;
+    clientUnminimized: QSignal;
+    clientMaximizeSet: QSignal;
+    clientActivated: QSignal;
+    clientFullScreenSet: QSignal;
+    numberDesktopsChanged: QSignal;
+    virtualScreenSizeChanged: QSignal;
+
+    clientArea(option: ClientAreaOption, screenNumber: number, desktopNumber: number);
+    clientList(): AbstractClient[];
+};
+
+type ClientAreaOption = any;
 type AbstractClient = any;
 type TopLevel = any;
 type X11Client = any;
