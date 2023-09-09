9b32caafdc924d4c6afbbcc21713bfe194a96752
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-09-09 12:58:56 +0200

    config/loader.ts: declare return type of `loadConfig`

--- src/config/loader.ts
+++ src/config/loader.ts
@@ -1,4 +1,4 @@
-function loadConfig() {
+function loadConfig(): Config {
     const config: any = {};
     for (const entry of configDef) {
         config[entry.name] = KWin.readConfig(entry.name, entry.default);
