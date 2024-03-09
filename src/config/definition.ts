687256d1dd6188e146d03136eebb815acbb43943
Peter Fajdiga <peter.fajdiga@gmail.com>
2024-03-09 19:08:48 +0100

    src/config: unjsonify

--- src/config/definition.ts
+++ src/config/definition.ts
@@ -72,103 +72,103 @@ const defaultWindowRules = `[
 
 const configDef = [
     {
-        "name": "gapsOuterTop",
-        "type": "UInt",
-        "default": 18
+        name: "gapsOuterTop",
+        type: "UInt",
+        default: 18,
     },
     {
-        "name": "gapsOuterBottom",
-        "type": "UInt",
-        "default": 18
+        name: "gapsOuterBottom",
+        type: "UInt",
+        default: 18,
     },
     {
-        "name": "gapsOuterLeft",
-        "type": "UInt",
-        "default": 18
+        name: "gapsOuterLeft",
+        type: "UInt",
+        default: 18,
     },
     {
-        "name": "gapsOuterRight",
-        "type": "UInt",
-        "default": 18
+        name: "gapsOuterRight",
+        type: "UInt",
+        default: 18,
     },
     {
-        "name": "gapsInnerHorizontal",
-        "type": "UInt",
-        "default": 18
+        name: "gapsInnerHorizontal",
+        type: "UInt",
+        default: 18,
     },
     {
-        "name": "gapsInnerVertical",
-        "type": "UInt",
-        "default": 18
+        name: "gapsInnerVertical",
+        type: "UInt",
+        default: 18,
     },
     {
-        "name": "manualScrollStep",
-        "type": "UInt",
-        "default": 200
+        name: "manualScrollStep",
+        type: "UInt",
+        default: 200,
     },
     {
-        "name": "manualResizeStep",
-        "type": "UInt",
-        "default": 600
+        name: "manualResizeStep",
+        type: "UInt",
+        default: 600,
     },
     {
-        "name": "offScreenOpacity",
-        "type": "UInt",
-        "default": 100
+        name: "offScreenOpacity",
+        type: "UInt",
+        default: 100,
     },
     {
-        "name": "untileOnDrag",
-        "type": "Bool",
-        "default": true
+        name: "untileOnDrag",
+        type: "Bool",
+        default: true,
     },
     {
-        "name": "stackColumnsByDefault",
-        "type": "Bool",
-        "default": false
+        name: "stackColumnsByDefault",
+        type: "Bool",
+        default: false,
     },
     {
-        "name": "resizeNeighborColumn",
-        "type": "Bool",
-        "default": false
+        name: "resizeNeighborColumn",
+        type: "Bool",
+        default: false,
     },
     {
-        "name": "reMaximize",
-        "type": "Bool",
-        "default": false
+        name: "reMaximize",
+        type: "Bool",
+        default: false,
     },
     {
-        "name": "skipSwitcher",
-        "type": "Bool",
-        "default": false
+        name: "skipSwitcher",
+        type: "Bool",
+        default: false,
     },
     {
-        "name": "scrollingLazy",
-        "type": "Bool",
-        "default": true
+        name: "scrollingLazy",
+        type: "Bool",
+        default: true,
     },
     {
-        "name": "scrollingCentered",
-        "type": "Bool",
-        "default": false
+        name: "scrollingCentered",
+        type: "Bool",
+        default: false,
     },
     {
-        "name": "scrollingGrouped",
-        "type": "Bool",
-        "default": false
+        name: "scrollingGrouped",
+        type: "Bool",
+        default: false,
     },
     {
-        "name": "tiledKeepBelow",
-        "type": "Bool",
-        "default": true
+        name: "tiledKeepBelow",
+        type: "Bool",
+        default: true,
     },
     {
-        "name": "floatingKeepAbove",
-        "type": "Bool",
-        "default": false
+        name: "floatingKeepAbove",
+        type: "Bool",
+        default: false,
     },
     {
-        "name": "windowRules",
-        "type": "String",
-        "default": defaultWindowRules
+        name: "windowRules",
+        type: "String",
+        default: defaultWindowRules,
     }
 ];
